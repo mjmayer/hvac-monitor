6e2f0232c7ab71f294b87c80a7aa9650d93acccf
Michael Mayer <mjmayer@gmail.com>
2025-07-03 20:56:49 -0700

    Read temp sensor by address

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Create `src/secrets.h` file with the following contents
 const char *ssid = "myssid";
 const char *password = "mywifipassword";
 ```
+
+## Temperature Sensor Setup
+
+Change the following lines in main.cpp to reflect the addresses of your temperature sensors.
+If you do not know the addresses of the sensor, they will be printed when you first run
+this program.
+
+```cpp
+DeviceAddress sensorBeforeCoil = {0x28, 0x58, 0x34, 0x50, 0x00, 0x00, 0x00, 0xD2};
+DeviceAddress sensorAfterCoil = {0x28, 0x32, 0x5A, 0x51, 0x00, 0x00, 0x00, 0xE2};
+DeviceAddress sensorAttic = {0x28, 0x0D, 0x70, 0x54, 0x00, 0x00, 0x00, 0xEF};
+```
--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,11 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+// Define the addresses of the sensors
+DeviceAddress sensorBeforeCoil = {0x28, 0x58, 0x34, 0x50, 0x00, 0x00, 0x00, 0xD2};
+DeviceAddress sensorAfterCoil = {0x28, 0x32, 0x5A, 0x51, 0x00, 0x00, 0x00, 0xE2};
+DeviceAddress sensorAttic = {0x28, 0x0D, 0x70, 0x54, 0x00, 0x00, 0x00, 0xEF};
+
 void setup()
 {
   Serial.begin(115200);
@@ -30,26 +35,63 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
+    yield(); // Allow other tasks to run
     Serial.print(".");
   }
 
+  while (WiFi.localIP() == IPAddress(0, 0, 0, 0))
+  {
+    delay(100);
+  }
   Serial.println("");
-  Serial.println("WiFi connected.");
+  Serial.println("\nWiFi connected.");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP().toString());
+
   sensors.begin();
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount());
+  Serial.println(" DS18B20 sensor(s).");
+
+  DeviceAddress addr;
+
+  for (uint8_t i = 0; i < sensors.getDeviceCount(); i++)
+  {
+    if (sensors.getAddress(addr, i))
+    {
+      Serial.print("Sensor ");
+      Serial.print(i);
+      Serial.print(" Address: ");
+      for (uint8_t j = 0; j < 8; j++)
+      {
+        if (addr[j] < 16)
+          Serial.print("0");
+        Serial.print(addr[j], HEX);
+      }
+      Serial.println();
+    }
+  }
 }
 
 void loop()
 {
-  // Request temperature measurement
+  // Ask all sensors to take a temperature reading
   sensors.requestTemperatures();
 
-  // Fetch temperature in Celsius
-  float tempC = sensors.getTempCByIndex(0);
+  float tempBefore = sensors.getTempC(sensorBeforeCoil);
+  float tempAfter = sensors.getTempC(sensorAfterCoil);
+  float tempAttic = sensors.getTempC(sensorAttic);
 
-  Serial.print("Temperature: ");
-  Serial.print(tempC);
+  Serial.print("Before Coil: ");
+  Serial.print(tempBefore);
+  Serial.println(" °C");
+
+  Serial.print("After Coil: ");
+  Serial.print(tempAfter);
+  Serial.println(" °C");
+
+  Serial.print("Attic: ");
+  Serial.print(tempAttic);
   Serial.println(" °C");
 
   delay(2000); // Wait 2 seconds between reads
